4d4c2c4368eaebea622534c1f3db9c408f9c06e0
hklinski <hklinski@edu.cdv.pl>
2021-03-08 15:27:42 +0100

    Add project files.

---
+++ WebApplication1/Controllers/PeopleController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            var people = new List<Models.Person>
+            {
+                new Models.Person
+                {
+                    FirstName = "Hubert",
+                    LastName = "Klinski",
+                    Id = 1
+                }
+            };
+            return Ok(people);
+
+        }
+    
+    }
+
+}
---
+++ WebApplication1/PeopleDb.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class PeopleDb : DbContext
+    {
+        public PeopleDb(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Person> People { get; set; }
+    }
+}
